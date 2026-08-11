58e2d30c12e58bcff0e55d3dd61d75a37cdceaf4
Bruh-Codes <kamasahdickson20@gmail.com>
2026-08-11 09:41:14 +0000

    chore: sync local development changes

--- db/migrate/20260811000000_update_default_branding_to_salescom.rb
+++ db/migrate/20260811000000_update_default_branding_to_salescom.rb
@@ -2,9 +2,15 @@ class UpdateDefaultBrandingToSalescom < ActiveRecord::Migration[7.1]
   def up
     installation_configs = Class.new(ActiveRecord::Base) do
       self.table_name = 'installation_configs'
+      serialize :serialized_value, coder: YAML, type: ActiveSupport::HashWithIndifferentAccess, default: {}.with_indifferent_access
     end
 
-    installation_configs.where(name: %w[INSTALLATION_NAME BRAND_NAME], value: 'Chatwoot').update_all(value: 'Salescom')
+    installation_configs.where(name: %w[INSTALLATION_NAME BRAND_NAME]).find_each do |config|
+      next unless config.serialized_value[:value] == 'Chatwoot'
+
+      config.serialized_value = config.serialized_value.merge(value: 'Salescom').with_indifferent_access
+      config.save!
+    end
   end
 
   def down
